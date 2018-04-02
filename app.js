26904997e15a47ae17a6867431c62051b506edf6
Dameon1 <dameonmedoza@gmail.com>
2018-04-02 12:35:55 -0700

    added catch for typeof

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-let myTeam = "Dameon and Suman";
+'use strict';
+let myTeam = 'Dameon and Suman';
 console.log(myTeam);
 console.log('Hello World');
 console.log('We did it.');
 
 function yearOfBirth(age){
-    if(age < 0 ){
-        throw new Error("Age cannot be negative");
-    }
-    else{
-        let yearOfBirth = 2018 - age;
-        return yearOfBirth;
-    }    
+  if(age < 0 ){
+    throw new Error('Age cannot be negative');
+  }
+  else{
+    let yearOfBirth = 2018 - age;
+    return yearOfBirth;
+  }    
 }
 
 function whoAmI(name,age){
-    console.log(`My name is ${name} and I'm ${age} years old`);
-    let yob = yearOfBirth(age);
-    console.log("I was born in " + yob);
+  if (typeof name !== 'string' || isNaN(age)){
+    console.log('Arguments not valid');
+  }
+  console.log(`My name is ${name} and I'm ${age} years old`);
+  let yob = yearOfBirth(age);
+  console.log('I was born in ' + yob);
 }
 
-whoAmI('suman',-5);
+whoAmI(5,5);
