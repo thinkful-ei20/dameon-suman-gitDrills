d585fd15b577a6dff313e7ccf08d9bc9a505e5c5
Dameon1 <dameonmedoza@gmail.com>
2018-04-02 12:08:32 -0700

    made function anonomous

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log('We did it.');
 
 
 
-function whoAmI(){
-    console.log('My name is Dameon and I'm 29 years old');
+function whoAmI(name,age){
+    console.log(`My name is ${name} and I'm ${age} years old`);
 }
 whoAmI();
