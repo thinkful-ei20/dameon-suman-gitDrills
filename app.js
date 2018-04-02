73af9ac817563d586115074c8340e8e64f41ad71
Suman Bandita <sumanbandita@gmail.com>
2018-04-02 15:17:10 -0400

    calculating age

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 let myTeam = "Dameon and Suman";
-
 console.log(myTeam);
 console.log('Hello World');
 console.log('We did it.');
-
-
-
-
 function whoAmI(name,age){
     console.log(`My name is ${name} and I'm ${age} years old`);
+    let yearOfBirth = 2018 - age; 
+    console.log("I was born in " + yearOfBirth);
 }
-whoAmI();
+
+whoAmI('suman',29);
