62e771b57d4b6a4a34e513616d39f12b5d577136
Dameon1 <dameonmedoza@gmail.com>
2018-04-02 12:23:47 -0700

    added yearOfBirth function

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ let myTeam = "Dameon and Suman";
 console.log(myTeam);
 console.log('Hello World');
 console.log('We did it.');
+
+function yearOfBirth(age){
+    let yearOfBirth = 2018 - age;
+    return yearOfBirth;
+}
+
 function whoAmI(name,age){
     console.log(`My name is ${name} and I'm ${age} years old`);
-    let yearOfBirth = 2018 - age; 
-    console.log("I was born in " + yearOfBirth);
+    let yob = yearOfBirth(age);
+    console.log("I was born in " + yob);
 }
 
 whoAmI('suman',29);
